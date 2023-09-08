c650270b9066b22e395e0a00e334c3517c63e7b9
Abhishek Roy <abhishekroy8475@gmail.com>
2023-09-08 15:17:26 +0530

    server: database connected

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/connectDb.js";
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
+connectDb();
 
 app.get("/", (req, res) => {
   res.send("Api Running Successfully!");
 });
 
-app.listen(5000, () => {
-    console.log("Server Running on PORT: 5000")
-})
+app.listen(PORT, () => {
+  console.log(`Server Running on PORT: ${PORT}`);
+});
