e3e3435693946d0840a076d0b13b6aac3dba6bb9
Abhishek Roy <abhishekroy8475@gmail.com>
2023-09-09 18:33:38 +0530

    client: search drawer added

--- client/src/components/chats/navbar/Search.jsx
+++ client/src/components/chats/navbar/Search.jsx
@@ -1,12 +1,45 @@
-import { Search2Icon } from "@chakra-ui/icons";
-import { Button } from "@chakra-ui/react";
+import { Search2Icon, SearchIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  IconButton,
+  Input,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 const Search = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
-      <Button rightIcon={<Search2Icon />} colorScheme="blue">
+      <Button rightIcon={<Search2Icon />} colorScheme="blue" onClick={onOpen}>
         Search Users...
       </Button>
+
+      <Drawer isOpen={isOpen} placement="left" size="sm" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Search Users To Chat</DrawerHeader>
+          <DrawerBody>
+            <Flex>
+              <Input rounded="full" placeholder="Type here..." />
+              <IconButton
+                isRound={true}
+                icon={<SearchIcon />}
+                colorScheme="blue"
+                ml={2}
+              />
+            </Flex>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
