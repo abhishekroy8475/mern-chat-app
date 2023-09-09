e285bd3fa13bbcbdf8622232b5de7686f9d97fde
Abhishek Roy <abhishekroy8475@gmail.com>
2023-09-09 11:58:17 +0530

    server: added error handler, register route done

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,1 +1,36 @@
-export const registerUser = async (req, res) => {};
+import ErrorHandler from "../utils/errorHandler.js";
+import User from "../models/userModel.js";
+
+export const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password, photo } = req.body;
+
+    if (!name || !email || !password) {
+      return next(new ErrorHandler("Please Fill All The Fields!", 400));
+    }
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      return next(new ErrorHandler("User Already Exist, Login Instead!", 400));
+    }
+
+    const newUser = await User.create({ name, email, password, photo });
+
+    if (newUser) {
+      return res.status(201).json({
+        name: newUser.name,
+        email: newUser.email,
+        _id: newUser._id,
+        photo: newUser.photo,
+      });
+    } else {
+      return next(
+        new ErrorHandler("Failed To Create User, Try After Sometime!", 500)
+      );
+    }
+  } catch (error) {
+    // sends response to client, NEED TO HANDLE!
+    return next(error);
+  }
+};
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/connectDb.js";
 import userRoutes from "./routes/userRoutes.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,9 @@ const PORT = process.env.PORT || 5000;
 connectDb();
 
 app.use("/api/v1/user", userRoutes);
+
+//Error Middleware
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server Running on PORT: ${PORT}`);
